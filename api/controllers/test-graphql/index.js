d0ab8687167a926e290443f086c11f1b14bf176f
poruruba <44470781+poruruba@users.noreply.github.com>
2021-03-29 22:32:09 +0900

    Update index.js

--- api/controllers/test-graphql/index.js
+++ api/controllers/test-graphql/index.js
@@ -3,7 +3,6 @@
 exports.graphql = async(obj, args, context, info) =>{
 	console.log("args", JSON.stringify(args));
 	console.log("path", info.path);
-	console.log("fieldName", info.fieldName);
 
 	return "Hello World";
 };
