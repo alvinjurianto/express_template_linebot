b8bf415645066ed840db7c022913d966ddd6b327
poruruba <44470781+poruruba@users.noreply.github.com>
2021-08-22 10:17:03 +0900

    Update routing_mqtt.js

--- api/controllers/routing_mqtt.js
+++ api/controllers/routing_mqtt.js
@@ -2,10 +2,11 @@
 
 const THIS_BASE_PATH = process.env.THIS_BASE_PATH;
 const CONTROLLERS_BASE = THIS_BASE_PATH + '/api/controllers/';
-const MQTT_TARGET_FNAME = "mqtt.json";
 
+const MQTT_BROKER_URL = process.env.MQTT_BROKER_URL;
+
+const MQTT_TARGET_FNAME = "mqtt.json";
 const DEFAULT_HANDLER = "handler";
-const broker = process.env.MQTT_BROKER_URL;
 
 const fs = require('fs');
 const mqtt = require('mqtt')
@@ -13,7 +14,7 @@ const mqtt = require('mqtt')
 let mqtt_client;
 
 function parse_mqtt() {
-  if( broker ){
+  if( MQTT_BROKER_URL ){
     let topic_list = [];
     mqtt_client = mqtt.connect(broker);
     mqtt_client.on('connect', () =>{
@@ -65,9 +66,9 @@ function parse_mqtt() {
       topic_list.forEach( item => {
         // topicの判別
         try{
-          var result = topic.match(item.regexp);
+          const result = topic.match(item.regexp);
           if(result){
-            var context = { topic: topic, mqtt: mqtt_client, isBase64Encoded: item.isBase64Encoded };
+            const context = { topic: topic, mqtt: mqtt_client, isBase64Encoded: item.isBase64Encoded };
             if (item.isBase64Encoded )
               item.proc(message.toString('base64'), context);
             else
