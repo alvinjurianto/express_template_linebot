4e17a694fbbc57ba579221119d371dfcb9c8a313
Alvin <alvinjr1910@gmail.com>
2022-07-14 16:17:33 +0900

    test

--- api/controllers/callback/index.js
+++ api/controllers/callback/index.js
@@ -72,12 +72,13 @@ exports.handler = async (event, context, callback) => {
     };
     try {
       const response = await fetch(
-        "https://nnlife-jp--irisdev04.my.salesforce.com/services/oauth2/token",
+        "/services/oauth2/token",
         {
           method: "post",
           body: JSON.stringify(body),
           headers: {
-            Host: "https://nnlife-jp--irisdev04.my.salesforce.com",
+            "Host": "https://nnlife-jp--irisdev04.my.salesforce.com",
+            "Host": "https://test.salesforce.com",
             "Content-Type": "application/x-www-form-urlencoded",
             "Content-length": "307",
           },
